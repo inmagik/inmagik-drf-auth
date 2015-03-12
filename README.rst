f11a8953821ec5d5ffd1fca2a7e329e9b289a974
Mauro Bianchi <bianchimro@gmail.com>
2015-03-12 12:44:31 +0100

    first commit

---
+++ README.rst
@@ -0,0 +1,17 @@
+================
+inmagik-drf-auth
+================
+
+Additional views and helpers that we always use with django rest framework
+
+
+Quick start
+-----------
+
+1. Add "inmagik_drf_auth" to your INSTALLED_APPS setting like this::
+
+    INSTALLED_APPS = (
+        ...
+        'inmagik_drf_auth',
+    )
+
---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='inmagik-drf-auth',
+    version='0.1',
+    packages=['inmagik_drf_auth'],
+    include_package_data=True,
+    license='BSD License',  # example license
+    description='Additional views and helpers for django-rest-framework authentication',
+    long_description=README,
+    url='http://www.example.com/',
+    author='Mauro Bianchi',
+    author_email='bianchimro@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License', # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
